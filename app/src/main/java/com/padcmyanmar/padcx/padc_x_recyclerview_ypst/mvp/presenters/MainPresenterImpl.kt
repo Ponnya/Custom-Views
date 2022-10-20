651092fec83ab99862b89faafb3808c9cb72324c
Ponnya <mr.ponnya@gmail.com>
2022-10-20 19:33:05 +0630

    -reuse composite view and handle it's action

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/mvp/presenters/MainPresenterImpl.kt
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/mvp/presenters/MainPresenterImpl.kt
@@ -1,5 +1,6 @@
 package com.padcmyanmar.padcx.padc_x_recyclerview_ypst.mvp.presenters
 
+import android.util.Log
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.data.models.NewsModelImpl
@@ -31,4 +32,26 @@ class MainPresenterImpl : MainPresenter, AbstractBasePresenter<MainView>() {
             if (it.isEmpty()) mView?.displayEmptyView() else mView?.displayNewsList(it)
         })
     }
+
+    /**
+     * ReactionViewPods.Delegate callback method
+     */
+    override fun onTapLike() {
+        Log.d("TAG", "onTapLike")
+    }
+
+    /**
+     * ReactionViewPods.Delegate callback method
+     */
+    override fun onTapComment() {
+        Log.d("TAG", "onTapComment")
+    }
+
+    /**
+     * ReactionViewPods.Delegate callback method
+     */
+    override fun onTapShare() {
+        Log.d("TAG", "onTapShare")
+    }
+
 }
