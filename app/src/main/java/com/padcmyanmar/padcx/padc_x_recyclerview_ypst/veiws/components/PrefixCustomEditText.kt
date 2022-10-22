5a03984218ed7bdc7bc6fe4a585cf12475e1e17d
Ponnya <mr.ponnya@gmail.com>
2022-10-22 22:54:24 +0630

    -add custom attributes to PrefixCustomEditText

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/PrefixCustomEditText.kt
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/PrefixCustomEditText.kt
@@ -6,6 +6,8 @@ import android.graphics.Color
 import android.graphics.Rect
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.withStyledAttributes
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
 
 class PrefixCustomEditText @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
@@ -15,6 +17,13 @@ class PrefixCustomEditText @JvmOverloads constructor(
     private var mPrefixColor = Color.BLACK
 
     private val mPrefixRect = Rect()
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.PrefixCustomEditText) {
+            mPrefix = getString(R.styleable.PrefixCustomEditText_prefixText) ?: mPrefix
+            mPrefixColor = getColor(R.styleable.PrefixCustomEditText_prefixTextColor, mPrefixColor)
+        }
+    }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         //1. place the prefix text
