fd454ea9be24d57c3bf7baa49db1f9cfada51887
Ponnya <mr.ponnya@gmail.com>
2022-10-26 15:17:30 +0630

    -state progress view (Composite Custom View)

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/activities/ModifyCustomViewActivity.kt
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/activities/ModifyCustomViewActivity.kt
@@ -2,16 +2,51 @@ package com.padcmyanmar.padcx.padc_x_recyclerview_ypst.activities
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.veiws.viewpods.StateProgressViewPod
+import kotlinx.android.synthetic.main.activity_modify_custom_view.*
 
 class ModifyCustomViewActivity : BaseActivity() {
-    companion object{
-        fun newIntent(context: Context) = Intent(context,ModifyCustomViewActivity::class.java)
+    private lateinit var mViewPodStateProgress: StateProgressViewPod
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, ModifyCustomViewActivity::class.java)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modify_custom_view)
+        setUpViewPod()
+
+        setListener()
+    }
+
+    private fun setUpViewPod() {
+        mViewPodStateProgress = viewPodStateProgress as StateProgressViewPod
+        mViewPodStateProgress.apply {
+            setStepDescription("Survey", "Cabling", "Splicing", "Activate")
+        }
+    }
+
+    private fun setListener() {
+
+        btn1.setOnClickListener {
+            mViewPodStateProgress.onTapStep1()
+        }
+
+        btn2.setOnClickListener {
+            mViewPodStateProgress.onTapStep2()
+        }
+
+        btn3.setOnClickListener {
+            mViewPodStateProgress.onTapStep3()
+        }
+
+        btn4.setOnClickListener {
+            mViewPodStateProgress.onTapStep4()
+            mViewPodStateProgress.completeAllSteps()
+        }
+
     }
 }
